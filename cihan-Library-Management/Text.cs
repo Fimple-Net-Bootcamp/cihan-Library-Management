2c82e5d6bb1a355ae45706067def8904eca5e988
clemexyo <ncihan@sabanciuniv.edu>
2023-11-30 20:46:55 +0300

    add Text abstract class

---
+++ cihan-Library-Management/Text.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management
+{
+    internal abstract class Text
+    {
+        //common properties
+        protected string? Title { get; set; }
+        protected string? Author { get; set; }
+        protected int? PublishYear { get; set; }
+    }
+}
