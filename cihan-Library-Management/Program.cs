5ca99b45674ce5ce625b4d7db2c5360946ec8f36
clemexyo <ncihan@sabanciuniv.edu>
2023-11-30 21:11:48 +0300

    add default books and members

--- cihan-Library-Management/Program.cs
+++ cihan-Library-Management/Program.cs
@@ -5,66 +5,112 @@ class Program
 {
     static void Main()
     {
-        string[] data = {
-            "Frank Herbert_Dune_1965"
-        };
-
         Library library = new Library();
+        Console.WriteLine("if you wish to populate your library with default books and members type 'default', anything else will lead you to a different process.\n");
+        string initialChoice = Console.ReadLine();
 
-        Console.WriteLine("Before we begin, let's populate the library with some books. Please enter author name, book name, and the year of publication" +
+        if (initialChoice == "default")
+        {
+            string[] booksData = 
+            {
+                "Frank Herbert_Dune_1965",
+                "Frank Herbert_Dune Messiah_1969",
+                "Frank Herbert_Children of Dune_1976",
+                "Frank Herbert_God Emperor of Dune_1981",
+                "Frank Herbert_Heretics of Dune_1984",
+                "Frank Herbert_Chapterhouse:Dune_1985",
+                "Christopher Tolkien_The Hobbit_1937",
+                "Christopher Tolkien_The Fellowship of the Ring_1954",
+                "Christopher Tolkien_The Two Towers_1954",
+                "Christopher Tolkien_The Return of the King_1955",
+                "Christopher Tolkien_Silmarillion_1977",
+            };
+
+            string[] membersData =
+            {
+                "Cihan-Nesvat",
+                "Billie-Eilish",
+                "Jensen-Ackles",
+                "Dua-Lipa"
+            };
+
+            int bookID = 1;
+            foreach (string s in booksData)
+            {
+                string[] data = s.Split('_');
+                Book book = new Book(bookID, data[0], data[1], Int32.Parse(data[2]));
+                bookID++;
+                library.AddBook(book);
+            }
+
+            int membershipNO = 1;
+            foreach(string s in membersData)
+            {
+                string[] data = s.Split("-");
+                Member member = new Member(data[0], data[1], membershipNO);
+                membershipNO++; 
+                library.AddMember(member);
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Let's first populate the library with some books. Please enter author name, book name, and the year of publication" +
                           " in a single line and separate with '-'.\n" +
                           "ex: Frank Herbert-Dune-1965\n");
 
-        //begin adding books to the library.
-        int bookID = 1;
-        while (true)
-        {
-            string bookDetails = Console.ReadLine();
-
-            if (bookDetails?.ToLower() == "exit") break;
-
-            string[] detailsList = bookDetails?.Split("-");
-
-            if (detailsList != null && detailsList.Length == 3 && int.TryParse(detailsList[2], out int year))
+            //begin adding books to the library.
+            int bookID = 1;
+            while (true)
             {
-                Book book = new Book(bookID, detailsList[0], detailsList[1], year);
-                library.AddBook(book);
-                bookID++;
-                Console.WriteLine("Book added\n");
+                string bookDetails = Console.ReadLine();
+
+                if (bookDetails?.ToLower() == "exit") break;
+
+                string[] detailsList = bookDetails?.Split("-");
+
+                if (detailsList != null && detailsList.Length == 3 && int.TryParse(detailsList[2], out int year))
+                {
+                    Book book = new Book(bookID, detailsList[0], detailsList[1], year);
+                    library.AddBook(book);
+                    bookID++;
+                    Console.WriteLine("Book added\n");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter details in the correct format.\n");
+                }
             }
-            else
+            //end adding books.
+
+            Console.WriteLine("**********************************************");
+
+            Console.WriteLine("Now let's add members for our library\n" +
+                "Add members in the format Name-Surname. Type exit to exit.\n");
+
+            //begin adding members to the library.
+            int membershipNo = 1;
+            while (true)
             {
-                Console.WriteLine("Invalid input. Please enter details in the correct format.\n");
+                string memberDetails = Console.ReadLine();
+
+                if (memberDetails?.ToLower() == "exit") break;
+                string[] detailsList = memberDetails?.Split("-");
+
+                if (detailsList != null && detailsList.Length == 2 && detailsList[1] != string.Empty)
+                {
+                    Member member = new Member(detailsList[0], detailsList[1], membershipNo);
+                    membershipNo++;
+                    library.AddMember(member);
+                    Console.WriteLine("Member added\n");
+                }
+                else Console.WriteLine("Invalid input. Please enter details in the correct format.\n");
             }
+            //end adding members.
+
+            Console.WriteLine("**********************************************\n");
         }
-        //end adding books.
 
-        Console.WriteLine("**********************************************");
-
-        Console.WriteLine("Now let's add members for our library\n" +
-            "Add members in the format Name-Surname. Type exit to exit.\n");
-
-        //begin adding members to the library.
-        int membershipNo = 1;
-        while (true)
-        {
-            string memberDetails = Console.ReadLine();
-
-            if (memberDetails?.ToLower() == "exit") break;
-            string[] detailsList = memberDetails?.Split("-");
-
-            if (detailsList != null && detailsList.Length == 2 && detailsList[1] != string.Empty)
-            {
-                Member member = new Member(detailsList[0], detailsList[1], membershipNo);
-                membershipNo++;
-                library.AddMember(member);
-                Console.WriteLine("Member added\n");
-            }
-            else Console.WriteLine("Invalid input. Please enter details in the correct format.\n");
-        }
-        //end adding members.
-
-        Console.WriteLine("**********************************************\n");
 
         //simulation begins
         Console.WriteLine("Ready to begin!\n");
